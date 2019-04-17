f69c8d0872538efac2dc6ace976b2ec9d1f2a62a
CRAZY <674015283@qq.com>
2019-04-17 20:52:55 +0800

    两数之和

---
+++ find_sum.py
@@ -0,0 +1,14 @@
+def find_sum(lis, target):
+    answer_dict = {}
+    answer = []
+    for item in lis:
+        if (target - item) not in answer_dict:
+            answer_dict[item] = target - item
+        else:
+            answer.append([item, target - item])
+            answer_dict.pop(target - item)
+    return answer
+
+
+lis = [1, 3, 34, 5, 6, 8, 4, 2, 2]
+print(find_sum(lis, 7))
