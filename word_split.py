5e451794305c566115ad4e258cfca32cdb609fee
CRAZYShimakaze <674015283@qq.com>
2019-04-23 16:34:25 +0800

    单词拆分

--- word_split.py
+++ word_split.py
@@ -1,24 +1,20 @@
-def wordBreak(s, dic):
+def wordSplit(s, dic):
     # write your code here
     # dic = set(dic)
-
-    if not s:
+    if s == '':
         return True
-    if not dic:
+    if dic == None:
         return False
-
-    arr = [False] * (len(s) + 1)
-    arr[0] = True
-    maxlen = max([len(d) for d in dic])
-    for i in range(1, len(s) + 1):
-        for word in dic:
-            if i - len(word) >= 0 and arr[
-                    i - len(word)] and word == s[i - len(word):i]:
-                arr[i] = True
+    answer = [False] * (len(s) + 1)
+    answer[0] = True
+    for i in range(1, len(s)+1):
+        for item in dict:
+            if i - len(item) >= 0 and answer[i - len(item)] and item == s[i-len(item):i]:
+                answer[i] = True
                 break
-    return arr[-1]
+    return answer[-1]
 
 
 s = "github"
 dict = ["git", "gi", "hu", "h", "b"]
-print(wordBreak(s, dict))
+print(wordSplit(s, dict))
