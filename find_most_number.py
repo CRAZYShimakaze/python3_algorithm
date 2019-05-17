97346099c2fb74151f0906215a28c580039c3124
CRAZYShimakaze <674015283@qq.com>
2019-05-17 17:53:46 +0800

    求众数

---
+++ find_most_number.py
@@ -0,0 +1,16 @@
+def find_most_number(array):
+    number = array[0]
+    cnt = 1
+    for item in array[1:]:
+        if number == item:
+            cnt += 1
+        elif cnt - 1 == 0:
+            number = item
+            cnt = 1
+        else:
+            cnt -= 1
+    return number
+
+
+list_ = [1, 1, 2, 3, 1, 2, 2, 2]
+print(find_most_number(list_))
