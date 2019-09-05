529feb678247f97ed460bcbb55b283e272aeac8f
CRAZYShimakaze <674015283@qq.com>
2019-09-05 23:10:51 +0800

    update

---
+++ random7-random10.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+@Description: random7生成random10
+@Date: 2019-09-05 20:51:08
+@Author: CRAZYShimakaze
+'''
+import random
+x = 41
+while x>=40:
+    x = (random.randint(1, 7)-1) * 10 + random.randint(1, 7)-1
+print(x//4+1)
+
+
