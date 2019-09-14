18d279c7793b1486acfc152caddf3c1de37ed3f0
CRAZYShimakaze <674015283@qq.com>
2019-09-14 18:48:26 +0800

    update

--- 最大连续子序列和.py
+++ 最大连续子序列和.py
@@ -2,20 +2,14 @@
 '''
 @Description:给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
 @Date: 2019-09-14 18:15:23
-@Author: CRAZYShimakaze
+@Author: leichaojian
 '''
 
 
-def maxSubArray(nums) -> int:
-    maxx = nums[0]
-    summ = 0
-    for item in nums:
-        summ = summ + item
-        if summ > maxx:
-            maxx = summ
-        if summ < 0:
-            summ = 0
-    return maxx
+def maxSubArray(nums):
+    for i in range(1, len(nums)):
+        nums[i] = max(nums[i], nums[i] + nums[i-1])
+        return max(nums)
 
 
 maxSubArray([-2, 1])
